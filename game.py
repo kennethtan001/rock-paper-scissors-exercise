95b3ac82d305b16fb120f69253c836cab967bf5a
kennethtan001 <kennethtan001@gmail.com>
2022-01-31 01:07:53 -0500

    main game script

---
+++ game.py
@@ -0,0 +1,43 @@
+
+
+import random
+
+print("-------------------")
+print("Welcome 'Player One' to my Rock-Paper-Scissors game...")
+print("-------------------")
+userChoice = input("Please choose either 'rock', 'paper', or 'scissors': ")
+userChoice = userChoice.lower()
+if (userChoice == "rock"):
+    print("You chose: ",userChoice)
+elif (userChoice == "paper"):
+    print("You chose: ",userChoice)
+elif (userChoice == "scissors"):
+    print("You chose: ",userChoice)
+else:
+    print("Sorry that is not a valid option")
+    exit()
+
+
+randomizer = [1, 2, 3] 
+if (random.choice(randomizer) == 1):
+    computerChoice = "rock"
+elif (random.choice(randomizer) == 2):
+    computerChoice = "paper"
+else:
+    computerChoice = "scissors"
+print ("Computer Choice: ", computerChoice)
+print("-------------------")
+
+
+if (userChoice == computerChoice):
+    print("Game is a tie!")
+if((userChoice == "rock" and computerChoice == "paper") or (userChoice == "paper" and 
+computerChoice == "scissors") or (userChoice == "scissors" and computerChoice == "rock")):
+    print("Oh, the computer won. It's ok.")
+    print("-------------------")
+    print("Thanks for playing. Please play again!")
+if((userChoice == "paper" and computerChoice == "rock") or (userChoice == "scissors" and 
+computerChoice == "paper") or (userChoice == "rock" and computerChoice == "scissors")):
+    print("Congrats, you won!")
+    print("-------------------")
+    print("Thanks for playing. Please play again!")
